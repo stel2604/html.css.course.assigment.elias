a953cd688d26da2ecea80e169588a6f023b2de29
stel2604 <114502020+stel2604@users.noreply.github.com>
2023-05-14 16:41:07 +0200

    14.5

---
+++ main.js
@@ -0,0 +1,46 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active')
+    navbar.classList.remove('active')
+
+}
+
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#shopping-cart').onclick = () =>{
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.toggle('active');
+    loginForm.classList.remove('active')
+    navbar.classList.remove('active')
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active')
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active')
+    
+
+}
+
+window.onscrollk= () =>{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active')
+    navbar.classList.remove('active')
+
+}
